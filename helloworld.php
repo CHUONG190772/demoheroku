10365145266ec20da4ffd9498a230c08a61a0467
VU BINH CHUONG <chuongvbgch190772@fpt.edu.vn>
2022-09-22 16:35:07 +0700

    ok

--- helloworld.php
+++ helloworld.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=\, initial-scale=1.0">
-    <title>Heroku</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Greenwich University</title>
     <style>
-        h1{
+        h1 {
             color: red;
         }
     </style>
 </head>
+
 <body>
-    <h1> VIETNAM</h1>
+    <h1>Welcome to Greenwich University</h1>
+
 </body>
+
 </html>
